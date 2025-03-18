c59eb95e19bcf22bb07ec89d89aad6a3144e6338
koms0109 <koms0109@postech.ac.kr>
2025-03-18 08:32:23 -0700

    feat: implement WrappingInt32

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,21 +1,14 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
-
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
+
+constexpr uint64_t ALIGN = 1ULL << 32;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + n;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +22,14 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t offset = static_cast<uint32_t>(n-isn);
+    const uint64_t aligned_checkpoint = (checkpoint / ALIGN) << 32;
+    const uint64_t mid = aligned_checkpoint + offset;
+    if (mid < checkpoint) {
+        const uint64_t high = aligned_checkpoint + offset + ALIGN;
+        return checkpoint < (ALIGN -1) * ALIGN && high - checkpoint <= checkpoint - mid ? high : mid;
+    } else {
+        const uint64_t low = aligned_checkpoint + offset - ALIGN;
+        return checkpoint >= ALIGN && mid - checkpoint >= checkpoint - low ? low : mid;
+    }
 }
